351d871478632e5517039ded3d6b5ce4114c1529
Diab <emailcodydiab@gmail.com>
2020-05-10 08:25:55 -0500

    added inquirer as dependency

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name,github] = profileDataArgs;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const [name,github] = profileDataArgs;
 
 
 
 
-fs.writeFile('index.html', generatePage(name,github), err => {
-    if (err) throw err;
+// fs.writeFile('index.html', generatePage(name,github), err => {
+//     if (err) throw err;
 
 
-console.log ('Porfolio complete! Check out index.html to see the output')
+// console.log ('Porfolio complete! Check out index.html to see the output')
 
-})
+// })
+
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+      {
+          type: 'input',
+          name: 'name',
+          message: 'What is your name?'
+      }
+  ])
+  .then(answers=> console.log(answers));
