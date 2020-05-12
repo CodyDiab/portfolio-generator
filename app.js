30b3e4a7f8d183c99982df7c8b2ed84f99936bad
Diab <emailcodydiab@gmail.com>
2020-05-12 07:53:22 -0500

    completed template literal

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 // const profileDataArgs = process.argv.slice(2, process.argv.length);
 // const [name,github] = profileDataArgs;
 
 
 
 
-// fs.writeFile('index.html', generatePage(name,github), err => {
-//     if (err) throw err;
-
-
-// console.log ('Porfolio complete! Check out index.html to see the output')
-
-// })
 
 const inquirer = require('inquirer');
 
@@ -148,7 +141,16 @@ const promptProject = portfolioData => {
   promptUser()
      .then(promptProject)
      .then(portfolioData => {
-         console.log(portfolioData);
+         const pageHTML = generatePage(portfolioData);
+
+         fs.writeFile('index.html', pageHTML, err => {
+            if (err) throw err;
+        
+        
+        console.log ('Porfolio complete! Check out index.html to see the output')
+        
+        })
+        
      });
 
  
