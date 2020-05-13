d56ba3ea7fc0c15b87f1db39ff37e2388b9318f9
Diab <emailcodydiab@gmail.com>
2020-05-13 07:34:45 -0500

    finished page generated

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-const fs = require('fs');
+const { writeFile,copyFile} = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template.js');
 // const profileDataArgs = process.argv.slice(2, process.argv.length);
 // const [name,github] = profileDataArgs;
@@ -141,16 +141,41 @@ const promptProject = portfolioData => {
   promptUser()
      .then(promptProject)
      .then(portfolioData => {
-         const pageHTML = generatePage(portfolioData);
-
-         fs.writeFile('index.html', pageHTML, err => {
-            if (err) throw err;
-        
-        
-        console.log ('Porfolio complete! Check out index.html to see the output')
-        
-        })
-        
+         return generatePage(portfolioData);
+     })
+     .then(pageHTML => {
+         return writeFile(pageHTML);
+     })
+     .then(writeFileResponse => {
+         console.log(writeFileResponse);
+         return copyFile();
+     })
+     .then(copyFileResponse => {
+         console.log(copyFileResponse);
+     })
+     .catch(err=> {
+         console.log(err);
      });
+
+
+    //      fs.writeFile('./dist/index.html', pageHTML, err => {
+    //         if (err) throw err;
+        
+        
+    //     console.log ('Porfolio complete! Check out index.html to see the output')
+        
+    //     fs.copyFile('./src/style.css','./dist/style.css', err => {
+    //         if (err) {
+    //             console.log(err);
+    //             return;
+    //         }
+    //         console.log('Style sheet copied successfully!');
+    //     });
+        
+    
+    //    });
+
+        
+    //  });
 
  
